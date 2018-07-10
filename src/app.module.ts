e468e31256a85f337febb69b660e2190b02f814f
Adrian Carriger <hello@adriancarriger.com>
2018-07-10 12:33:02 -0500

    feat(app): add graphql server

---
+++ src/app.module.ts
@@ -0,0 +1,58 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AppResolvers } from './app.resolver';
+import { SubscriptionsService } from './subscriptions/subscriptions.service';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { Prisma } from './generated/prisma';
+
+@Module({
+  imports: [GraphQLModule, SubscriptionsModule.forRoot()],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    AppResolvers,
+    {
+      provide: Prisma,
+      useFactory: () => {
+        return new Prisma({
+          endpoint: 'http://prisma:4466',
+          debug: true,
+          secret: 'dev-secret-123',
+        });
+      },
+    },
+  ],
+})
+export class ApplicationModule implements NestModule {
+  constructor(
+    private readonly graphQLFactory: GraphQLFactory,
+    private readonly subscriptionsService: SubscriptionsService,
+  ) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('src/**/*.graphql');
+    const schema = this.graphQLFactory.createSchema({ typeDefs });
+
+    this.subscriptionsService.createSubscriptionServer(schema);
+
+    consumer
+      .apply(
+        graphiqlExpress({
+          endpointURL: '/graphql',
+          subscriptionsEndpoint: `ws://localhost:5001/subscriptions`,
+        }),
+      )
+      .forRoutes('/graphiql')
+      .apply(
+        graphqlExpress(req => ({
+          schema,
+          rootValue: req,
+          context: req,
+        })),
+      )
+      .forRoutes('/graphql');
+  }
+}
