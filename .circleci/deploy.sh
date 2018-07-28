9ec107ddc0596b042f1d959226e879faf5d67bec
Adrian Carriger <hello@adriancarriger.com>
2018-07-28 11:26:00 -0500

    fix(deploy): add endpoints and secrets

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -17,4 +17,7 @@ EOF
 heroku container:login
 docker tag $IMAGE_ID registry.heroku.com/$HEROKU_TARGET/web
 docker push registry.heroku.com/$HEROKU_TARGET/web
+heroku config:set -a $HEROKU_TARGET \
+  PRISMA_DATA_ENDPOINT=$PRISMA_DATA_ENDPOINT \
+  PRISMA_SECRET=$PRISMA_SECRET
 heroku container:release web -a $HEROKU_TARGET
