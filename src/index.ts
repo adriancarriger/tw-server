82b91be2442fd4301175dfcb813eb372387f3db7
Adrian Carriger <hello@adriancarriger.com>
2018-08-05 14:10:28 -0500

    refactor(resolvers): move resolvers out of index

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga';
-import { Prisma } from './generated/prisma';
 import * as dotenv from 'dotenv';
+
+import { Prisma } from './generated/prisma';
+import { resolvers } from './resolvers';
 
 // Load .env
 dotenv.config();
 
-const {
-  PORT = 4332, PRISMA_DATA_ENDPOINT, PRISMA_SECRET
-} = process.env;
-
-const resolvers = {
-  Query: {
-    feed(parent, args, ctx, info) {
-      return ctx.db.query.posts({
-          where: {
-            isPublished: true
-          }
-        },
-        info
-      );
-    },
-    drafts(parent, args, ctx, info) {
-      return ctx.db.query.posts({
-          where: {
-            isPublished: false
-          }
-        },
-        info
-      );
-    },
-    post(parent, {
-      id
-    }, ctx, info) {
-      return ctx.db.query.post({
-          where: {
-            id
-          }
-        },
-        info
-      );
-    }
-  },
-  Mutation: {
-    createDraft(parent, {
-      title,
-      text
-    }, ctx, info) {
-      return ctx.db.mutation.createPost({
-          data: {
-            title,
-            text
-          }
-        },
-        info
-      );
-    },
-    deletePost(parent, {
-      id
-    }, ctx, info) {
-      return ctx.db.mutation.deletePost({
-          where: {
-            id
-          }
-        },
-        info
-      );
-    },
-    publish(parent, {
-      id
-    }, ctx, info) {
-      return ctx.db.mutation.updatePost({
-          where: {
-            id
-          },
-          data: {
-            isPublished: true
-          }
-        },
-        info
-      );
-    }
-  }
-};
+const { PORT = 4332, PRISMA_DATA_ENDPOINT, PRISMA_SECRET } = process.env;
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: request => ({
+  context: (request) => ({
     ...request,
     db: new Prisma(<any>{
       typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
@@ -97,8 +23,4 @@ const server = new GraphQLServer({
   })
 });
 
-server.start({
-    port: PORT
-  },
-  () => console.log(`Server is running on http://localhost:${PORT}`)
-);
+server.start({ port: PORT }, () => console.log(`Server is running on http://localhost:${PORT}`));
---
+++ src/resolvers/queries.ts
@@ -0,0 +1,32 @@
+export const Query = {
+  feed(parent, args, ctx, info) {
+    return ctx.db.query.posts(
+      {
+        where: {
+          isPublished: true
+        }
+      },
+      info
+    );
+  },
+  drafts(parent, args, ctx, info) {
+    return ctx.db.query.posts(
+      {
+        where: {
+          isPublished: false
+        }
+      },
+      info
+    );
+  },
+  post(parent, { id }, ctx, info) {
+    return ctx.db.query.post(
+      {
+        where: {
+          id
+        }
+      },
+      info
+    );
+  }
+};
